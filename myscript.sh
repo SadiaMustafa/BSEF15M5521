dc205df861fb6d9269e95e8222f9eed1bfadaf79
SadiaMustafa <bsef15m521@pucit.edu.pk>
2019-03-07 22:16:33 +0500

    Add files via upload

---
+++ myscript.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+#answers the questions in their seactions with propoer numbered label and in order ... beware linus is case-senstive
+
+## basic shell commands 
+
+1. whoami
+2. sudo touch /tmp/testingFile
+
+## questions
+
+
+## absolute or relative path
